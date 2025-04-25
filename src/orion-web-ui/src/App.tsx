9808ba193aa03a773b82e258e737d3daaaaba4c1
squid <squid@stormwind.it>
2025-04-25 18:58:09 +0200

    feat(orion-web-ui): add postcss configuration for tailwindcss and autoprefixer feat(orion-web-ui): add App component with Button and DefaultLayout in src/App.tsx feat(orion-web-ui): add icons for Logo, Discord, Twitter, Github, Moon, Sun, Heart, Search feat(orion-web-ui): add Navbar component with theme switch in src/components/navbar.tsx feat(orion-web-ui): add ThemeSwitch component for light/dark mode in src/components/theme-switch.tsx feat(orion-web-ui): add site configuration for name, description, nav items, links feat(orion-web-ui): add DefaultLayout component for consistent layout in src/layouts/DefaultLayout.tsx feat(orion-web-ui): add IconSvgProps type in src/types/index.ts feat(orion-web-ui): add tailwindcss configuration in tailwind.config.js

--- src/orion-web-ui/src/App.tsx
+++ src/orion-web-ui/src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { observer } from "mobx-react-lite"
 import { useStore } from './stores/rootStore'
-
+import { Button } from "@heroui/button"
+import DefaultLayout from "./layouts/DefaultLayout"
 
 
 const App = observer(() => {
-  const [count, setCount] = useState(0)
 
   const rootStore = useStore()
 
@@ -15,27 +12,10 @@ const App = observer(() => {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>{rootStore.authStore.counter}</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <DefaultLayout>
+        <h1>{rootStore.authStore.counter}</h1>
+        <Button>Press me</Button>
+      </DefaultLayout>
     </>
   )
 })
