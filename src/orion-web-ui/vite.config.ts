a0574e7341a8300de100be152b5093426ba5e0ec
squid <squid@stormwind.it>
2025-04-25 17:24:04 +0200

    feat(orion-web-ui): update package.json dependencies and devDependencies feat(orion-web-ui): remove unused App.css and index.css files feat(orion-web-ui): refactor App.tsx to remove import of deleted App.css feat(orion-web-ui): add Provider component for context management feat(orion-web-ui): add global styles using Tailwind CSS in globals.css feat(orion-web-ui): update Vite configuration to use vite-tsconfig-paths plugin

--- src/orion-web-ui/vite.config.ts
+++ src/orion-web-ui/vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  optimizeDeps: {
-    include: ['@emotion/styled'],
-  },
+  plugins: [react(), tsconfigPaths()],
   build: {
     outDir: '../Orion.Server/Assets/Web',
     emptyOutDir: true,
