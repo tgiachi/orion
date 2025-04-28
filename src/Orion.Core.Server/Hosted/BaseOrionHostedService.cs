a5436044aec475acf991210b7db9d4362672f3eb
squid <squid@stormwind.it>
2025-04-28 15:25:28 +0200

    feat(ApplicationBuilderExtension.cs): add support for defaultDirectories parameter in InitApplication method to allow specifying default directories feat(BaseOrionHostedService.cs): create BaseOrionHostedService class to provide common functionality for hosted services feat(Orion.Core.Server.csproj): add Microsoft.Extensions.Hosting.Abstractions package reference for BaseOrionHostedService feat(OrionHostedService.cs): extend BaseOrionHostedService in OrionHostedService to leverage common functionality and customize startup and shutdown behavior

---
+++ src/Orion.Core.Server/Hosted/BaseOrionHostedService.cs
@@ -0,0 +1,110 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orion.Core.Server.Events.Server;
+using Orion.Core.Server.Interfaces.Services.Base;
+using Orion.Core.Server.Interfaces.Services.System;
+using Orion.Core.Server.Internal.Services;
+
+namespace Orion.Core.Server.Hosted;
+
+public abstract class BaseOrionHostedService : IHostedService
+{
+    private readonly List<ServiceDefinitionObject> _serviceDefinitions;
+
+    protected IServiceProvider ServiceProvider { get; }
+
+    protected IEventBusService EventBusService { get; }
+
+    protected ILogger Logger { get; }
+
+    protected BaseOrionHostedService(
+        ILogger<BaseOrionHostedService> logger, List<ServiceDefinitionObject> serviceDefinitions,
+        IEventBusService eventBusService, IServiceProvider serviceProvider
+    )
+    {
+        _serviceDefinitions = serviceDefinitions;
+        EventBusService = eventBusService;
+        ServiceProvider = serviceProvider;
+        Logger = logger;
+    }
+
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await BeforeStartAsync();
+
+        foreach (var serviceDef in _serviceDefinitions)
+        {
+            try
+            {
+                var service = ServiceProvider.GetService(serviceDef.ServiceType);
+                if (service == null)
+                {
+                    Logger.LogError("Service {ServiceName} not registered", serviceDef.ServiceType.Name);
+                    throw new InvalidOperationException($"Service {serviceDef.ServiceType.Name} not registered");
+                }
+
+                Logger.LogDebug("Starting service {ServiceName}", serviceDef.ServiceType.Name);
+
+                if (service is IOrionStartService orionService)
+                {
+                    await orionService.StartAsync(cancellationToken);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "Error starting service {ServiceName}", serviceDef.ServiceType.Name);
+                throw;
+            }
+        }
+
+        await EventBusService.PublishAsync(new ServerStartedEvent(), cancellationToken);
+        await EventBusService.PublishAsync(new ServerReadyEvent(), cancellationToken);
+
+        await OnReady();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await EventBusService.PublishAsync(new ServerStoppingEvent(), cancellationToken);
+
+        await OnStopping();
+
+        foreach (var serviceDef in _serviceDefinitions)
+        {
+            try
+            {
+                var service = ServiceProvider.GetService(serviceDef.ServiceType);
+                if (service == null)
+                {
+                    Logger.LogError("Service {ServiceName} not registered", serviceDef.ServiceType.Name);
+                    continue;
+                }
+
+                Logger.LogDebug("Stopping service {ServiceName}", serviceDef.ServiceType.Name);
+
+
+                if (service is IOrionStartService orionService)
+                {
+                    await orionService.StopAsync(cancellationToken);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "Error stopping service {ServiceName}", serviceDef.ServiceType.Name);
+            }
+        }
+    }
+
+    protected virtual async Task OnReady()
+    {
+    }
+
+    protected virtual async Task OnStopping()
+    {
+    }
+
+    protected virtual async Task BeforeStartAsync()
+    {
+    }
+}
