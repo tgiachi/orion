3f15d6a5237a821fac21c32ac008407f4b8bf7df
squid <squid@stormwind.it>
2025-04-28 14:22:31 +0200

    feat(DefaultOrionScriptsModule.cs): add new module for registering script services feat(DefaultOrionServiceModule.cs): add new module for registering service implementations feat(JsLoggerModule.cs): add JsLoggerModule for logging in JavaScript scripts refactor(DefaultServicesModule.cs): refactor service registration to remove duplicate services and improve organization feat(Program.cs): add DefaultOrionServiceModule and DefaultOrionScriptsModule to service registration in Program class

---
+++ src/Orion.Core.Server/Modules/Container/DefaultOrionServiceModule.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.DependencyInjection;
+using Orion.Core.Server.Extensions;
+using Orion.Core.Server.Interfaces.Modules;
+using Orion.Core.Server.Interfaces.Services.System;
+using Orion.Core.Server.Services;
+
+namespace Orion.Core.Server.Modules.Container;
+
+public class DefaultOrionServiceModule : IOrionContainerModule
+{
+    public IServiceCollection RegisterServices(IServiceCollection services)
+    {
+        return services
+            .AddService<IEventBusService, EventBusService>()
+            .AddService<IDiagnosticService, DiagnosticService>()
+            .AddService<ITextTemplateService, TextTemplateService>()
+            .AddService<ISchedulerSystemService, SchedulerSystemService>()
+            .AddService<IEventDispatcherService, EventDispatcherService>()
+            .AddService<IScriptEngineService, ScriptEngineService>()
+            .AddService<IProcessQueueService, ProcessQueueService>();
+    }
+}
