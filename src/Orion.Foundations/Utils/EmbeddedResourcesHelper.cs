e5105a92594c124e1b4105c93dac9b38891a9eb7
squid <squid@stormwind.it>
2025-05-01 19:34:33 +0200

    feat(Orion.Core.Server): add overloaded method to AddService extension for easier service registration feat(Orion.Foundations): add EmbeddedResourcesHelper utility class for managing embedded resources feat(Orion.Foundations): add ResourceUtils class with methods for working with embedded resources feat(Orion.Server): update Scalar.AspNetCore package reference to version 2.2.5

---
+++ src/Orion.Foundations/Utils/EmbeddedResourcesHelper.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Prima.Core.Server.Utils;
+
+/// <summary>
+/// Utility class for managing embedded resources in the assembly
+/// </summary>
+public static partial class EmbeddedResourcesHelper
+{
+
+}
