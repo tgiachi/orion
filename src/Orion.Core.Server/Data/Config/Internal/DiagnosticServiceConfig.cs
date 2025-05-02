e903927284cf145c446082b0903d40807f724116
squid <squid@stormwind.it>
2025-05-02 11:24:25 +0200

    feat(DiagnosticServiceConfig.cs): add PidFileName property to DiagnosticServiceConfig class with default value refactor(OrionServicesExtensions.cs): change priority value when adding DiagnosticService to services refactor(DiagnosticService.cs): update PidFilePath assignment to use PidFileName from DiagnosticServiceConfig feat(Program.cs): add DiagnosticService to services in Program class

--- src/Orion.Core.Server/Data/Config/Internal/DiagnosticServiceConfig.cs
+++ src/Orion.Core.Server/Data/Config/Internal/DiagnosticServiceConfig.cs
@@ -3,4 +3,6 @@ namespace Orion.Core.Server.Data.Config.Internal;
 public class DiagnosticServiceConfig
 {
     public int MetricsIntervalInSeconds { get; set; } = 60;
+
+    public string PidFileName { get; set; } = "server.pid";
 }
